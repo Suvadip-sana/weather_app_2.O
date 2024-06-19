147a709845237463627330de81d10bf5fe085c1e
Suvadip Sana <suvadipsana602@gmail.com>
2024-06-19 06:16:23 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+const API_KEY = "8c3ff98b73c4ee6e1cd07024e57d4141";
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
