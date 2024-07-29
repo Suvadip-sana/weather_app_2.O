130fc6993731279d966c339aa06940878414ccd1
Suvadip Sana <suvadipsana602@gmail.com>
2024-07-30 00:32:23 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,31 @@
+const API_KEY = "8c3ff98b73c4ee6e1cd07024e57d4141";
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const dayImages = ['dayImage/background1.jpg', 'dayImage/background2.jpg', 'dayImage/background3.webp', 'dayImage/background4.jpg', 'dayImage/background5.jpg', 'dayImage/background6.jpg', 'dayImage/background7.jpg', 'dayImage/background8.jpg',];
+
+const nightImages = ['nightImage/background9.jpg', 'nightImage/background10.jpg', 'nightImage/background11.webp', 'nightImage/background12.jpg', 'nightImage/background13.jpg', 'nightImage/background14.jpg', 'nightImage/background15.jpg', 'nightImage/background16.jpg',];
+
+
+
